1d602e27ebcef6fd7ccc98739b3016f91d4b84c4
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-19 14:36:30 +0300

    Added a search bar

--- js/script.js
+++ js/script.js
@@ -1,3 +1,37 @@
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Working well');
+    displayproduct()
     }) 
+
+    function displayproduct(){
+     let customersearch= document.getElementById("search").value; 
+     customersearch= customersearch.toLowerCase();
+     let products= document.getElementsByClassName("categories");
+
+     for (i = 0; i < products.length; i++) {
+		if (!products[i].innerHTML.toLowerCase().includes(input)) {
+			products[i].style.display="none";
+		}
+		else {
+			products[i].style.display="Trending-Products";				
+		}
+	}
+}
+
+
+
+
+//     // JavaScript code
+// function search_animal() {
+// 	let input = document.getElementById('searchbar').value
+// 	input=input.toLowerCase();
+// 	let x = document.getElementsByClassName('animals');
+	
+// 	for (i = 0; i < x.length; i++) {
+// 		if (!x[i].innerHTML.toLowerCase().includes(input)) {
+// 			x[i].style.display="none";
+// 		}
+// 		else {
+// 			x[i].style.display="list-item";				
+// 		}
+// 	}
+// }
